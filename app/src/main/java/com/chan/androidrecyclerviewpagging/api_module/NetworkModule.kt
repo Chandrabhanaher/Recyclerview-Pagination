56e89cf2dd75dbc5ab9a082be02d26198e05684f
Chandrabhan Aher <chandrabhanaher@gmail.com>
2023-03-28 10:26:07 +0530

    pagination android

--- app/src/main/java/com/chan/androidrecyclerviewpagging/api_module/NetworkModule.kt
+++ app/src/main/java/com/chan/androidrecyclerviewpagging/api_module/NetworkModule.kt
@@ -1,12 +1,13 @@
 package com.chan.androidrecyclerviewpagging.api_module
 
 import com.google.gson.GsonBuilder
+import okhttp3.CacheControl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Chandrabhan Haribhau Aher on 16-03-2023.
@@ -14,7 +15,8 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
  */
 object NetworkModule {
 
-    var SHOE_URL:String ="http://192.168.1.103/pgdemo/"
+    //var SHOE_URL:String ="http://192.168.1.103/pgdemo/"
+    var HOST_URL:String ="https://chandrabhanaher.000webhostapp.com/"
 
     val getClient: ApiInterface
         get() {
@@ -23,14 +25,30 @@ object NetworkModule {
                 .create()
 
             val interceptor = HttpLoggingInterceptor()
-            interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.BODY }
+            interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.NONE }
 
             val client = OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .addInterceptor{ chain ->
+                    val original  = chain.request()
+                    val requestBuilder = original.newBuilder()
+                        .addHeader("Content-Type", "application/json")
+                        .addHeader("Accept", "application/json")
+                        .method(original.method, original.body)
+                        .cacheControl(
+                            CacheControl.Builder()
+                                .maxAge(0, TimeUnit.SECONDS) //noCache()
+                                .build())
+                    val request = requestBuilder.build()
+                    chain.proceed(request)
+                }
                 .addInterceptor(interceptor)
                 .build()
 
             val retrofit = Retrofit.Builder()
-                .baseUrl(SHOE_URL)
+                .baseUrl(HOST_URL)
                 .client(client)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
